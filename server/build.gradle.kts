e1f88ce5c5e46886ec0cb363b3fce2397763de62
Vladislav <vladcheremnov@gmail.com>
2020-10-11 02:41:15 +0700

    Реализовал метод регистрации пользователя #3996 2h30m

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation(project(":api-model"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mail")
---
+++ server/src/main/kotlin/com/toosafinder/security/SecurityConfiguration.kt
@@ -0,0 +1,14 @@
+package com.toosafinder.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
